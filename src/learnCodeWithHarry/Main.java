d40330cc7a2cb995e0ca124f8c4a295ed1cf85a1
M-SabirHussain <superior.sabirhussain@gmail.com>
2024-08-22 09:28:52 +0500

    first commit

--- src/learnCodeWithHarry/Main.java
+++ src/learnCodeWithHarry/Main.java
@@ -2,6 +2,6 @@ package learnCodeWithHarry;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("First Commit of code with harry");
+        System.out.println("Install JDK and IntelliJ IDEA");
     }
 }
